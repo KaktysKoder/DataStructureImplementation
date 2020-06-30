8eb7ecc3b759714ab7fa96c3e645fcae7240d8ff
Rimuru <kaktysKoder@gmail.com>
2020-06-30 19:30:24 +0400

    Исправить завтра отловленные исключения

--- Stack/StackImplementation/Stack.cs
+++ Stack/StackImplementation/Stack.cs
@@ -1,11 +1,95 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Text;
 
 namespace StackImplementation
 {
-    internal sealed class Stack
+    internal sealed class Stack<T> : IStack<T>, IEnumerable<T>
     {
+        private readonly int _size;
+        private T[] _array;
+        private int _head;
+        private const int _defaulSize = 10;
 
+        public Stack()
+        {
+            _array = new T[_defaulSize];
+        }
+
+        public Stack(int size)
+        {
+            _size = size;
+            _head = 0;
+            _array = new T[size];
+        }
+
+        public int Top => _head;
+
+        public int Size => _size;
+
+        public int Count => _head;
+
+        public bool IsEmpty => _head == 0;
+
+        public bool IsFull => _head == _size;
+
+        public T Peek => _array[_head - 1];
+
+        [Obsolete("Нет проверки на IsEmpty")]
+        public T pop => _array[--_head];
+
+        public void Push(T element)
+        {
+            if (IsFull)
+                throw new InvalidOperationException("Stack Overflow");
+
+            /////////////////
+            if (_head == _array.Length)
+                Resize(_array.Length + 10);
+            ////////////////
+
+            _array[_head] = element;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Stack is empty");
+
+            T array = _array[--_head];
+
+            _array[_head] = default;
+
+            ////////////
+            if(-Count > 0 && _head < _array.Length - 10)
+                Resize(_array.Length - 10);
+            ////////////
+
+            return array;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (IsEmpty) throw new InvalidOperationException("Стек не заполнен.");
+
+            for (int i = _head; i > 0; i--)
+            {
+                yield return _array[i - 1];
+            }
+        }
+
+        private void Resize(int max)
+        {
+            T[] tempItem = new T[max];
+
+            for (int i = 0; i < _head; i++)
+            {
+                tempItem[i] = _array[i];
+            }
+
+            _array = tempItem;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
--- Stack/StackImplementation/StartProgram.cs
+++ Stack/StackImplementation/StartProgram.cs
@@ -6,8 +6,35 @@ namespace StackImplementation
     {
         private static void Main()
         {
+            TestStack1();
 
+        }
 
+        private static void TestStack1()
+        {
+            try
+            {
+                Stack<string> stack = new Stack<string>();
+
+                stack.Push("Shild");
+                stack.Push("CODEBLOG");
+                stack.Push("Vasya");
+                stack.Push("Jojo");
+
+                string head = stack.Pop();
+                Console.WriteLine(head);
+
+                // просто получаем верхушку стека без извлечения
+                head = stack.Peek;
+
+                Console.WriteLine(head);
+            }
+            catch (InvalidOperationException exc)
+            {
+                Console.WriteLine(exc.Message);
+                Console.WriteLine(exc.StackTrace);
+                Console.WriteLine(exc.TargetSite);
+            }
         }
     }
 }
