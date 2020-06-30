9829eb61c77515dcf08a2a29bfa773b692b4f7b5
Rimuru <kaktysKoder@gmail.com>
2020-06-30 08:21:25 +0400

    Изучение структуры данных Queue. Реализую с помощью односвязного списка.

---
+++ Queue/QueueImplementation/StartProgram.cs
@@ -0,0 +1,14 @@
+﻿//Очередь и её реализация с помощью односвязных списков
+
+using System;
+
+namespace QueueImplementation
+{
+    class StartProgram
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello Queue!");
+        }
+    }
+}
