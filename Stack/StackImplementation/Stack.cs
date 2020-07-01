aa463d31e88565a6ae1aeaa1d36aa7ba7c2e83df
Rimuru <kaktysKoder@gmail.com>
2020-07-01 09:24:52 +0400

    Убрал лишний интерфейс, лишний функционал.

--- Stack/StackImplementation/Stack.cs
+++ Stack/StackImplementation/Stack.cs
@@ -4,17 +4,18 @@ using System.Collections.Generic;
 
 namespace StackImplementation
 {
-    internal sealed class Stack<T> : IStack<T>, IEnumerable<T>
+    internal sealed class Stack<T> : IEnumerable<T>
     {
         private readonly int _size;
         private T[] _array;
         private int _head;
+
         private const int _defaulSize = 10;
 
-        public Stack()
-        {
-            _array = new T[_defaulSize];
-        }
+        //public Stack()
+        //{
+        //    _array = new T[_defaulSize];
+        //}
 
         public Stack(int size)
         {
@@ -23,50 +24,58 @@ namespace StackImplementation
             _array = new T[size];
         }
 
-        public int Top => _head;
+        //Маркер верхнего элемента стека
+        public int Top { get => _head; }
 
-        public int Size => _size;
+        //Размер стека
+        public int Size { get => _size; }
 
-        public int Count => _head;
+        //Количество элементов стека
+        public int Count { get => _head; }
 
+        //Проверить пустой ли стек
         public bool IsEmpty => _head == 0;
 
+        //Проверить заполнен ли стек
         public bool IsFull => _head == _size;
 
+        //Вернуть верхний элемент
         public T Peek => _array[_head - 1];
 
         [Obsolete("Нет проверки на IsEmpty")]
-        public T pop => _array[--_head];
+        public T Pop_ => _array[--_head];
+        public T Pop() => _array[--_head];
 
+        //Добавляем элемент в стек
         public void Push(T element)
         {
             if (IsFull)
                 throw new InvalidOperationException("Stack Overflow");
 
             /////////////////
-            if (_head == _array.Length)
-                Resize(_array.Length + 10);
+            //if (_head == _array.Length)
+            //    Resize(_array.Length + 10);
             ////////////////
 
-            _array[_head] = element;
+            _array[_head++] = element;
         }
 
-        public T Pop()
-        {
-            if (IsEmpty)
-                throw new InvalidOperationException("Stack is empty");
+        //public T Pop()
+        //{
+        //    if (IsEmpty)
+        //        throw new InvalidOperationException("Stack is empty");
 
-            T array = _array[--_head];
+        //    T array = _array[--_head];
 
-            _array[_head] = default;
+        //    _array[_head] = default;
 
-            ////////////
-            if(-Count > 0 && _head < _array.Length - 10)
-                Resize(_array.Length - 10);
-            ////////////
+        //    ////////////
+        //    if(-Count > 0 && _head < _array.Length - 10)
+        //        Resize(_array.Length - 10);
+        //    ////////////
 
-            return array;
-        }
+        //    return array;
+        //}
 
         public IEnumerator<T> GetEnumerator()
         {
@@ -78,17 +87,17 @@ namespace StackImplementation
             }
         }
 
-        private void Resize(int max)
-        {
-            T[] tempItem = new T[max];
+        //private void Resize(int max)
+        //{
+        //    T[] tempItem = new T[max];
 
-            for (int i = 0; i < _head; i++)
-            {
-                tempItem[i] = _array[i];
-            }
+        //    for (int i = 0; i < _head; i++)
+        //    {
+        //        tempItem[i] = _array[i];
+        //    }
 
-            _array = tempItem;
-        }
+        //    _array = tempItem;
+        //}
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
--- Stack/StackImplementation/StartProgram.cs
+++ Stack/StackImplementation/StartProgram.cs
@@ -6,7 +6,7 @@ namespace StackImplementation
     {
         private static void Main()
         {
-            TestStack1();
+            Test2();
 
         }
 
@@ -14,7 +14,7 @@ namespace StackImplementation
         {
             try
             {
-                Stack<string> stack = new Stack<string>();
+                Stack<string> stack = new Stack<string>(8);
 
                 stack.Push("Shild");
                 stack.Push("CODEBLOG");
@@ -28,6 +28,46 @@ namespace StackImplementation
                 head = stack.Peek;
 
                 Console.WriteLine(head);
+            }
+            catch (InvalidOperationException exc)
+            {
+                Console.WriteLine(exc.Message);
+                Console.WriteLine(exc.StackTrace);
+                Console.WriteLine(exc.TargetSite);
+            }
+        }
+
+        private static void Test2()
+        {
+            try
+            {
+                Console.Title = "Custom simple Stack in native C#";
+
+                Stack<string> s = new Stack<string>(5);
+                Console.WriteLine("Добавляем на склад коробки: 1, 2, 3...");
+                s.Push("первая коробка");
+                s.Push("вторая коробка");
+                s.Push("третья коробка");
+                Console.WriteLine("Всего коробок: " + s.Count.ToString());
+                Console.WriteLine();
+
+                Console.WriteLine("Достаём крайнюю коробку: " + s.Pop());
+                Console.WriteLine("Достаём крайнюю коробку: " + s.Pop());
+                Console.WriteLine("Узнать какая коробка осталась: " + s.Peek);
+                Console.WriteLine("Всего коробок: " + s.Count.ToString());
+                Console.WriteLine();
+
+                Console.WriteLine("Добавим ещё коробки: 4, 5...");
+                s.Push("четвертая коробка");
+                s.Push("пятая коробка");
+                Console.WriteLine("Всего коробок: " + s.Count.ToString());
+                Console.WriteLine();
+
+                Console.WriteLine("Переберём оставшиеся коробки на складе:");
+                foreach (string box in s)
+                {
+                    Console.WriteLine('-' + box);
+                }
             }
             catch (InvalidOperationException exc)
             {
